835995a05a1d9c58ad6ef67324bca7cb39ae4c9b
Samuel Javorka <samkoj@hotmail.co.uk>
2024-09-21 19:00:20 +0100

    useEffect with empty dependency array to only run on initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -13,13 +13,17 @@ export default function App() {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
 	const [pickedPlaces, setPickedPlaces] = useState([]);
 
-	navigator.geolocation.getCurrentPosition((position) => {
-		const sortedPlaces = sortPlacesByDistance(
-			AVAILABLE_PLACES,
-			position.coords.latitude,
-			position.coords.longitude
-		);
-	});
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const sortedPlaces = sortPlacesByDistance(
+				AVAILABLE_PLACES,
+				position.coords.latitude,
+				position.coords.longitude
+			);
+
+			setAvailablePlaces(sortedPlaces);
+		});
+	}, []);
 
 	function handleStartRemovePlace(id) {
 		modal.current.open();
@@ -63,7 +67,12 @@ export default function App() {
 					places={pickedPlaces}
 					onSelectPlace={handleStartRemovePlace}
 				/>
-				<Places title="Available Places" places={availablePlaces} onSelectPlace={handleSelectPlace} />
+				<Places
+					title="Available Places"
+					places={availablePlaces}
+					fallbackText="Sorting places by distance..."
+					onSelectPlace={handleSelectPlace}
+				/>
 			</main>
 		</>
 	);
