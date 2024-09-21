c0bb819b29f314cb1343f55eec0f360bf4bd0cef
Samuel Javorka <samkoj@hotmail.co.uk>
2024-09-21 19:46:59 +0100

    store data in localStorage
    example does not need useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,12 @@ export default function App() {
 			const place = AVAILABLE_PLACES.find((place) => place.id === id);
 			return [place, ...prevPickedPlaces];
 		});
+		// retrieve places saved in localStorage
+		const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+		// append new selected places to localStorage
+		if (storedIds.indexOf(id) === -1) {
+			localStorage.setItem("selectedPlaces", JSON.stringify([id, ...storedIds]));
+		}
 	}
 
 	function handleRemovePlace() {
